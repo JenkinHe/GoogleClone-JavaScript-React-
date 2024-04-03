14b393de71891bbabfedf8db73eb1f00c38bd229
Jenkibear <jenkin.he@students.mq.edu.au>
2024-04-03 22:54:28 +1100

    tailwind finally works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react';
+
+
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import { Routes } from './components/Routes';
 
 const App = () => {
+  const[darkTheme,setDarkTheme]=useState(false);
+
   return (
-    <div>
-        <h1>App</h1>
+    <div className={darkTheme ? 'dark':''}>
+        <div className="bg-gray-500">
+          App
+          </div>
     </div>
   )
 }
 
-export default App
+export default App;
