835afd3d950264d404452c6ce5058d62e98f13de
Jenkibear <jenkin.he@students.mq.edu.au>
2024-04-03 23:24:59 +1100

    button

--- src/App.js
+++ src/App.js
@@ -10,8 +10,10 @@ const App = () => {
 
   return (
     <div className={darkTheme ? 'dark':''}>
-        <div className="bg-gray-500">
-          App
+        <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+            <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme}></Navbar>
+            <Routes></Routes>
+            <Footer></Footer>
           </div>
     </div>
   )
