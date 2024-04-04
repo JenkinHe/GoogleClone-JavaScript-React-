faf8496425c6827f128aee0904d00e99d8d81227
Jenkibear <jenkin.he@students.mq.edu.au>
2024-04-04 11:05:30 +1100

    Realise im using stuff thats way to old to guide me using modern tools face slap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React,{useState} from 'react';
 
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
-import { Routes } from './components/Routes';
+import { Routess } from './components/Routes';
+
 
 const App = () => {
   const [darkTheme, setDarkTheme] = useState(false);
@@ -13,10 +14,11 @@ const App = () => {
     <div className={darkTheme ? 'dark' : ''}>
       <div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-200 min-h-screen">
         <Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme} />
-            <Routes></Routes>
+            <Routess></Routess>
             <Footer></Footer>
-            <div>{darkTheme ? 'Dark Mode Enabled' : 'Dark Mode Disabled'}</div>
+            
           </div>
+          
     </div>
   )
 }
--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import {BrowserRouter as Router,Routes,Route,Navigate} from 'react-router-dom';
+import { Results } from './Results';
 
-export const Routes = () => {
+export const Routess = () => {
   return (
-    <div>
-        <h1>Routes</h1>
+    <div className="p-4">
+        
+        
+          <Routes>
+        
+        <Route exact path="/" element={<Navigate replace to="/Search" />} />
+        <Route exact path ={['/search','/images','/news','/videos']} element={<Results/>}></Route>
+      </Routes>
+    
     </div>
   );
 }
